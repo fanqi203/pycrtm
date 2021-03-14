1. Create_profiles.py: Change how mixing ratio is averaged  and Fixed the bug to integrate mixing ratio and o3 2. Similar change in create_profiles_hafs.py 3. Pycrtm_gfs.py added more tests 4. Added two satellite related files
 On branch ydif
 Changes to be committed:
   (use "git reset HEAD <file>..." to unstage)

	modified:   create_profile.py
	modified:   create_profile_hafs.py
	new file:   de421.bsp
	new file:   goes.txt
	modified:   plot_images.py
	modified:   plot_pdf.py
	modified:   pycrtm_gfs.py
	modified:   submit.wk

testCases/create_profile.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import ipdb
+#ipdb.set_trace()
 import configparser
 import os, sys
 from matplotlib import pyplot as plt
@@ -96,6 +98,7 @@ def create_profile2d(f,fobs):
     # p levels  
     pint=gfs.lv_ISBL0
 
+
     # heights 
     hgt=gfs.HGT_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
 
@@ -117,9 +120,9 @@ def create_profile2d(f,fobs):
     # qp=gfs.lv_ISBL5
 
     # relative humidity
-    q=gfs.RH_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
+    qq=gfs.RH_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
     #  interploate q to all levels (upper levels has no moisture)
-    qpint=q.interp(lv_ISBL5=pint,kwargs={"fill_value": 0.0})
+    qpint=qq.interp(lv_ISBL5=pint,kwargs={"fill_value": 0.0})
 
     # temp 
     t = gfs.TMP_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
@@ -128,8 +131,9 @@ def create_profile2d(f,fobs):
     mix = mixing_ratio_from_relative_humidity(pint.broadcast_like(t),t,qpint)
     mix=xr.DataArray(mix,coords=t.coords,dims=t.dims)
     mix.attrs['units']='kg/kg'
-    mixavg=mix.rolling(lv_ISBL0=2,center=True).mean()[1:]
-
+    # test another way to average mix
+    #mixavg=mix.rolling(lv_ISBL0=2,center=True).mean()[1:]
+    mixavg=mix[1:,:,:]
     # get the t and p for the layers (instead of levels)
     tavg=t.rolling(lv_ISBL0=2,center=True).mean()[1:]
     dp=xr.DataArray(np.diff(pint),dims=pint.dims).broadcast_like(tavg)
@@ -139,9 +143,13 @@ def create_profile2d(f,fobs):
     pavg=-R*tavg*(1+mixavg*fv)*dp/dz/g
     rho=-(1+mixavg*fv)*dp/dz/g
 
+    mixavg=mixavg*rho*-dz
+
     # ozone and the five types of "clouds"
     o3=gfs.O3MR_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
     o3_pavg=o3.interp(lv_ISBL12=pavg.coords['lv_ISBL0'],kwargs={"fill_value": 0.0})
+
+    o3_pavg=o3_pavg*rho*-dz
 
     cld=gfs.CLWMR_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')
     cld_pavg=cld.interp(lv_ISBL7=pavg.coords['lv_ISBL0'],kwargs={"fill_value": 0.0})

testCases/create_profile_hafs.py
@@ -132,8 +132,9 @@ def create_profile2d(f):
     mix = mixing_ratio_from_relative_humidity(pint.broadcast_like(t),t,qpint)
     mix=xr.DataArray(mix,coords=t.coords,dims=t.dims)
     mix.attrs['units']='kg/kg'
-    mixavg=mix.rolling(lv_ISBL0=2,center=True).mean()[1:]
-
+    # test another way to average mix
+    #mixavg=mix.rolling(lv_ISBL0=2,center=True).mean()[1:]
+    mixavg=mix[1:,:,:]
     # get the t and p for the layers (instead of levels)
     tavg=t.rolling(lv_ISBL0=2,center=True).mean()[1:]
     dp=xr.DataArray(np.diff(pint),dims=pint.dims).broadcast_like(tavg)
@@ -142,6 +143,8 @@ def create_profile2d(f):
     g=9.8
     pavg=-R*tavg*(1+mixavg*fv)*dp/dz/g
     rho=-(1+mixavg*fv)*dp/dz/g
+
+    mixavg=mixavg*rho*-dz
 
     # ozone and the five types of "clouds"
     # o3=gfs.O3MR_P0_L100_GLL0.sel(lat_0=y0, lon_0=x0,method='nearest')

testCases/plot_images.py
@@ -5,7 +5,7 @@
 
 import xarray as xr
 import matplotlib
-matplotlib.use('AGG')# or PDF, SVG or PS
+#matplotlib.use('AGG')# or PDF, SVG or PS
  
 import matplotlib.pyplot as plt
 
@@ -56,6 +56,7 @@ def plot(cases, figname):
     x=xr.open_mfdataset(f0,preprocess=preprocess,concat_dim="cases")
     ch7_cntrl=0.5*(x.bt[0,6]+x.bt[0,7])
 
+
     fobs="profiles/obs_dorian*.nc"
     obs=xr.open_mfdataset(fobs,preprocess=preprocessobs,concat_dim="cases")
     ch7_obs=obs.obs
@@ -74,8 +75,8 @@ def plot(cases, figname):
     def cond1(var,st,ed):
         return np.where((var > st) & (var < ed), var, np.nan)
 
-    st=100
-    ed=220
+    st=300
+    ed=320
     ax0.pcolor(cond1(ch7_cntrl,st,ed), vmin=220, vmax=320)
     ax0.set_title('cntrl'+str(ch7_cntrl.mean().values))
 
@@ -94,16 +95,14 @@ def plot(cases, figname):
 #    legend.get_frame().set_facecolor('C0')
 #    plt.legend()
 #    fig.tight_layout()
-    plt.savefig(figname)
-#    plt.show()
+#    plt.savefig(figname)
+    plt.show()
 
 
 
 
 from pathlib import Path
 
-pathlist = Path('profiles').glob('profile2d4_2019_dorain_gfs_output*.nc')
-for path in pathlist:
-    cases=str(path) #"profiles/profile2d4_2019_dorain_gfs_output_test_T_sfc_min6*.nc"
-    figname=cases+'.level5.png' #"test.png"
-    plot(cases,figname)
+cases="profile2d4_2019_dorain_gfs_output_cntrl.nc"
+figname=cases+'.newq.png' #"test.png"
+plot(cases,figname)

testCases/plot_pdf.py
@@ -28,7 +28,7 @@ def preprocessobs(ds):
 
 def hist(ch7):
     ch7flat=ch7.where(ch7>0).values.flatten()
-    [obshist, obsbin] = np.histogram(ch7flat, density=True, bins=np.arange(190, 320,3))
+    [obshist, obsbin] = np.histogram(ch7flat, density=True, bins=np.arange(180, 320, 3))
     avg_BT=ch7flat[np.where(ch7flat>0)].mean()
     labelstr=str(np.round(avg_BT,0))+' '+str(ch7.cases.values)
     return [obsbin, obshist,  labelstr]
@@ -39,7 +39,7 @@ def plot(cases, figname):
     xall=xr.open_mfdataset(f2,preprocess=preprocess,concat_dim="cases")
     ch7=xall.bt
 
-    f0="profiles/profile2d4_2019_dorain_gfs_output_test_cntrl*.nc"
+    f0="profile2d4_2019_dorain_gfs_output_cntrl.nc"
     x=xr.open_mfdataset(f0,preprocess=preprocess,concat_dim="cases")
     ch7_cntrl=0.5*(x.bt[0,6]+x.bt[0,7])
 
@@ -58,17 +58,18 @@ def plot(cases, figname):
     for i in np.arange(ch7.shape[0]):
         ax.plot(hist(ch7[i,7])[0][1:],hist(ch7[i,7])[1][0:],label=hist(ch7[i,7])[2]) 
     
-    ax.plot(hist(ch7_cntrl)[0][1:],hist(ch7_cntrl)[1][0:],label=hist(ch7_cntrl)[2])
+    ax.plot(hist(ch7_cntrl)[0][1:],hist(ch7_cntrl)[1][0:],label="cntrl")
     ax.plot(hist(ch7_obs)[0][1:],hist(ch7_obs)[1][0:],label="obs")
     ax.plot(hist(ch7_sbt)[0][1:],hist(ch7_sbt)[1][0:],label="upp")
 
     legend=ax.legend(loc='best',shadow=False,fontsize='x-small')
     legend.get_frame().set_facecolor('C0')
     plt.legend()
-    plt.savefig(figname)
-    #plt.show()
+    #plt.savefig(figname)
+    plt.show()
 
 
-cases="profiles/profile2d4_2019_dorain_gfs_output_test_T_sfc_min6*.nc"
+cases="profile2d4_2019_dorain_gfs_output_test_land06*.nc"
+
 figname="test_t_sfc_min.png"
 plot(cases,figname)